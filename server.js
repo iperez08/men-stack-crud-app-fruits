fb80a16b118ef18a57a1fa2db213d1c9c0451bd6
Ismael P <iperez08@gmail.com>
2024-07-30 15:01:36 -0400

    create index route and view

--- server.js
+++ server.js
@@ -10,16 +10,34 @@ const app = express();
 
 mongoose.connect(process.env.MONGODB_URI)
 
+// read information from body of the request
+app.use(express.urlencoded({ extended: false }))
+
 app.get('/', async (req, res) => {
   res.render('index.ejs')
+})
+
+app.get('/fruits', async (req, res) => {
+  const fruits = await Fruit.find()
+  console.log(fruits)
+  res.render("fruits/index.ejs", { fruits } )
 })
 
 app.get("/fruits/new", async (req, res) => {
   res.render("fruits/new.ejs")
 })
 
+app.post("/fruits", async (req, res) => {
+  // talk to DB through the model
+  if (req.body.isReadyToEat) {
+    req.body.isReadyToEat = true
+  } else req.body.isReadyToEat = false
+  await Fruit.create(req.body)
+  res.redirect("/fruits")
+})
+
 // event listener that notifies you if server is connected to database
-mongoose.connection.on('connected', () =>{
+mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}`)
 })
 
