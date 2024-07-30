85c7592a4f69c5109dbbe4b86c2896050a910239
Ismael P <iperez08@gmail.com>
2024-07-30 13:16:23 -0400

    connect to database

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 // Here is where we import modules
 // We begin by loading Express
 import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+import mongoose, { mongo } from 'mongoose'
 
 const app = express();
 
+mongoose.connect(process.env.MONGODB_URI)
+
 app.get('/', async (req, res) => {
   res.render('index.ejs')
+})
+
+// event listener that notifies you if server is connected to database
+mongoose.connection.on('connected', () =>{
+  console.log(`Connected to MongoDB ${mongoose.connection.name}`)
 })
 
 app.listen(3000, () => {
