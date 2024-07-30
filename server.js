23dd61c43fd50c1367bb4f880f324665d0d4651b
Ismael P <iperez08@gmail.com>
2024-07-30 13:42:17 -0400

    create new.ejs and get route

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
 import mongoose, { mongo } from 'mongoose'
+import Fruit from './models/fruit.js'
 
 const app = express();
 
@@ -11,6 +12,10 @@ mongoose.connect(process.env.MONGODB_URI)
 
 app.get('/', async (req, res) => {
   res.render('index.ejs')
+})
+
+app.get("/fruits/new", async (req, res) => {
+  res.render("fruits/new.ejs")
 })
 
 // event listener that notifies you if server is connected to database
