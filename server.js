20d004565dd5519eef293dcaad05c9af8dd8f5b9
Ismael P <iperez08@gmail.com>
2024-07-30 16:11:02 -0400

    code update

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
 import mongoose, { mongo } from 'mongoose'
+import methodOverride from "method-override"
+import logger from "morgan"
 import Fruit from './models/fruit.js'
 
 const app = express();
@@ -12,6 +14,9 @@ mongoose.connect(process.env.MONGODB_URI)
 
 // read information from body of the request
 app.use(express.urlencoded({ extended: false }))
+// override post and get methods to delete or put methods
+app.use(methodOverride("_method"))
+app.use(logger("dev"))
 
 app.get('/', async (req, res) => {
   res.render('index.ejs')
@@ -19,12 +24,16 @@ app.get('/', async (req, res) => {
 
 app.get('/fruits', async (req, res) => {
   const fruits = await Fruit.find()
-  console.log(fruits)
   res.render("fruits/index.ejs", { fruits } )
 })
 
 app.get("/fruits/new", async (req, res) => {
   res.render("fruits/new.ejs")
+})
+
+app.get("/fruits/:fruitID", async (req, res) => {
+  const foundFruit = await Fruit.findById(req.params.fruitID)
+  res.render("fruits/show.ejs", { fruit: foundFruit })
 })
 
 app.post("/fruits", async (req, res) => {
@@ -35,6 +44,32 @@ app.post("/fruits", async (req, res) => {
   await Fruit.create(req.body)
   res.redirect("/fruits")
 })
+
+app.delete("/fruits/:fruitID", async (req, res) => {
+  await Fruit.findByIdAndDelete(req.params.fruitID)
+  res.redirect("/fruits")
+})
+
+app.get("/fruits/:fruitID/edit", async (req, res) => {
+  const fruit = await Fruit.findById(req.params.fruitID)
+  res.render("fruits/edit.ejs", { fruit: fruit })
+})
+
+app.put("/fruits/:fruitId", async (req, res) => {
+  // Handle the 'isReadyToEat' checkbox data
+  if (req.body.isReadyToEat) {
+    req.body.isReadyToEat = true;
+  } else {
+    req.body.isReadyToEat = false;
+  }
+  
+  // Update the fruit in the database
+  await Fruit.findByIdAndUpdate(req.params.fruitId, req.body);
+
+  // Redirect to the fruit's show page to see the updates
+  res.redirect(`/fruits/${req.params.fruitId}`);
+});
+
 
 // event listener that notifies you if server is connected to database
 mongoose.connection.on('connected', () => {
